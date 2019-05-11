d3be4662cf083010c82d35f837f22d89d2c9da82
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-05-10 21:39:46 -0300

    adding file for Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mainWebsite'));
+app.use(express.static(__dirname + '/dist/Website'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/mainWebsite/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
